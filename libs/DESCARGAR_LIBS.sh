9a438be7e6f333e2bd2324d1459b875332a1171f
practicas-hugosanz <hugosanzpracticas@gmail.com>
2026-03-27 13:27:56 +0100

    Delete libs directory

--- libs/DESCARGAR_LIBS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Ejecuta este script UNA VEZ para descargar las librerías necesarias
-echo "Descargando librerías..."
-curl -L "https://cdn.jsdelivr.net/npm/highlight.js@11/lib/highlight.min.js" -o highlight.min.js
-curl -L "https://cdn.jsdelivr.net/npm/highlight.js@11/styles/github-dark.min.css" -o github-dark.min.css
-curl -L "https://cdn.jsdelivr.net/npm/marked@9/marked.min.js" -o marked.min.js
-echo "¡Listo! Las librerías están descargadas."
